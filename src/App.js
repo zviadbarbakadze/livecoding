b26dc7eba3d5aba3a76195b2a850b34c7c146888
zviadbarbakadze <zviadbarbakadze5@gmail.com>
2023-05-20 22:54:50 +0400

    livecoding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 function App() {
+  const [x, setX] = useState();
+  const [y, setY] = useState();
+  useEffect(() => {
+    const update = (e) => {
+      setX(e.x);
+      setY(e.y);
+    };
+    // window.addEventListener("mousemove", update);
+    window.addEventListener("click", update);
+  }, [setX, setY]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Button>Rendo</Button>
+      <Button>Undo</Button>
+      <Div>{x}</Div>
+      <Div>{y}</Div>
+    </Container>
   );
 }
 
 export default App;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const Button = styled.button`
+  width: 100px;
+  height: 50px;
+  cursor: pointer;
+`;
+const Div = styled.div``;
